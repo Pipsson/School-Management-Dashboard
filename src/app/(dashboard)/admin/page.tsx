c110b670dab1cf3a5232880c23f407c2a410df44
Pipsson <juliusmallya8420@gmail.com>
2024-10-18 16:37:19 +0300

    react calendar added using react calendar package

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/app/components/AttendanceChart";
 import CountChart from "@/app/components/CountChart";
+import EventCalendar from "@/app/components/EventCalendar";
 import { FinanceChart } from "@/app/components/FinanceChart";
 import UserCard from "@/app/components/UserCard";
 import React from "react";
@@ -34,7 +35,9 @@ const AdminPage = () => {
       </div>
 
       {/* right */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col">
+        <EventCalendar />
+      </div>
     </div>
   );
 };
